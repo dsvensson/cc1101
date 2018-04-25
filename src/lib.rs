ae5c4285eedf02d98e05c313bedfb4addf1859f8
Daniel Svensson <dsvensson@gmail.com>
2018-04-25 20:55:10 +0000

    Add support for awaiting machine FSM target.

--- src/lib.rs
+++ src/lib.rs
@@ -148,6 +148,16 @@ where
         Ok(())
     }
 
+    fn await_machine_state(&mut self, target: MachineState) -> Result<(), E> {
+        loop {
+            let state = self.read_register(Register::MARCSTATE)? & 0b11111;
+            if target.value() == state {
+                break;
+            }
+        }
+        Ok(())
+    }
+
     pub fn receive_would_block(&mut self) -> Result<bool, E> {
         let rx_bytes = self.read_register(Register::RXBYTES)?;
         Ok(!((rx_bytes & 0x7F > 0) && (rx_bytes & 0x80 == 0)))
@@ -384,4 +394,39 @@ pub enum RadioMode {
     Receive,
     Transmit,
     Idle,
+}
+
+impl MachineState {
+    fn value(&self) -> u8 {
+        *self as u8
+    }
+}
+
+#[allow(dead_code)]
+#[allow(non_camel_case_types)]
+#[derive(Clone, Copy)]
+enum MachineState {
+    SLEEP = 0x00,
+    IDLE = 0x01,
+    XOFF = 0x02,
+    VCOON_MC = 0x03,
+    REGON_MC = 0x04,
+    MANCAL = 0x05,
+    VCOON = 0x06,
+    REGON = 0x07,
+    STARTCAL = 0x08,
+    BWBOOST = 0x09,
+    FS_LOCK = 0x0A,
+    IFADCON = 0x0B,
+    ENDCAL = 0x0C,
+    RX = 0x0D,
+    RX_END = 0x0E,
+    RX_RST = 0x0F,
+    TXRX_SWITCH = 0x10,
+    RXFIFO_OVERFLOW = 0x11,
+    FSTXON = 0x12,
+    TX = 0x13,
+    TX_END = 0x14,
+    RXTX_SWITCH = 0x15,
+    TXFIFO_UNDERFLOW = 0x16,
 }
